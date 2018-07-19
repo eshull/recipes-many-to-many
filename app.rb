e5b910603072c52023c663cb2f1801e61f41b173
Craig Wann, Eric Shull and Nick Brown <nick@>
2018-07-19 16:54:06 -0700

    final commit

--- app.rb
+++ app.rb
@@ -24,13 +24,32 @@ post("/recipes") do
   redirect("/recipes")
 end
 
+get ('/recipes/delete') do
+  recipe_id = params.fetch("recipe_delete")
+  recipe=Recipe.find(recipe_id)
+  recipe.delete
+  @recipes = Recipe.all
+  erb(:recipes)
+end
+
+get("/recipes/:id") do
+  @recipe = Recipe.find(params.fetch("id").to_i())
+  if @employee.project_id
+    @project = Project.find(@employee.project_id)
+  else
+    @project = nil
+  end
+  @projects = Project.all
+  erb(:recipes)
+end
+
 get("/ingredients") do
   @ingredients = Ingredient.all()
   erb(:ingredients)
 end
 
-post("/recipes") do
-  name = params.fetch("recipe_name")
-  Recipe.create({:name => name})
-  redirect("/recipes")
+post("/ingredients") do
+  name = params.fetch("ingredients")
+  Ingredients.create({:name => name})
+  redirect("/ingredients")
 end
